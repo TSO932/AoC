014bd01c72e28561832ef70a63635c2a6c02964f
Matthew Latto <matt.salesforce.jl@gmail.com>
2020-11-17 14:55:24 +0000

    WIP Day 10 Part 1

---
+++ Day10/Part1/Main.fs
@@ -0,0 +1,23 @@
+open System
+printfn "Hello, World!"
+
+
+let a = ".#..#
+.....
+#####
+....#
+...##"
+
+let arrays = a.Split '\010' |> Seq.map (fun a -> Array.ofSeq a) |> Array.ofSeq
+let matrix = Array2D.init arrays.Length arrays.[0].Length (fun i j -> arrays.[i].[j])
+
+let countAsteroids(y, x) = 
+    let up = matrix.[0..(y - 1), x] |> Array.exists ((=) '#')
+    let down = matrix.[(y + 1).., x] |> Array.exists ((=) '#')
+    let left = matrix.[y, 0..(x - 1)] |> Array.exists ((=) '#')
+    let right = matrix.[y, (x + 1)..] |> Array.exists ((=) '#')
+    if matrix.[y,x] = '#' then (if up then 1 else 0) + (if down then 1 else 0) + (if left then 1 else 0) + (if right then 1 else 0) else 0
+
+let matrix3 = matrix |> Array2D.mapi (fun y x a -> countAsteroids(y, x))
+
+printfn "%A" matrix3
