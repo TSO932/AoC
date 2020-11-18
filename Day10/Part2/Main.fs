ea751a955a5ad15c3d9db89bbe0c53be5ae7437d
Matthew Latto <matt.salesforce.jl@gmail.com>
2020-11-18 18:13:18 +0000

    Update Day10/Part2/Main.fs

--- Day10/Part2/Main.fs
+++ Day10/Part2/Main.fs
@@ -6,19 +6,19 @@ let vaporise(spaceMap:string,stationLocation) =
 
     if isDebug then printfn "%A" matrix
 
-    let matrixOfAngles = matrix |> Array2D.mapi (fun v h c -> (c = '#' && not (v = snd stationLocation && h = fst stationLocation), System.Math.Atan2(float (h - fst stationLocation),float (v - snd stationLocation))))
+    let matrixOfAngles = matrix |> Array2D.mapi (fun v h c -> (c = '#' && not (v = snd stationLocation && h = fst stationLocation), (System.Math.Atan2(float (h - fst stationLocation),float (v - snd stationLocation)), h - fst stationLocation)))
     
     let mutable firingAngle = float 3.141592654f
 
-    let angleWithMostAsteroids = matrixOfAngles |> Seq.cast<bool*double> |> Seq.filter (fun x -> fst x) |> Seq.groupBy (fun x -> snd x) |> Seq.map (fun (a,b) -> (a, Seq.length b)) |> Seq.maxBy snd |> fst
-    let nextAngleOfAttack = matrixOfAngles |> Seq.cast<bool*double> |> Seq.filter (fun x -> fst x) |> Seq.maxBy snd
-    printfn ">>>>>>>>>>>>>>>>>>>>>> %A" nextAngleOfAttack
+    let angleWithMostAsteroids = matrixOfAngles |> Seq.cast<bool*(double*int)> |> Seq.filter (fun x -> fst x) |> Seq.groupBy (fun x -> fst (snd x)) |> Seq.map (fun (a,b) -> (a, Seq.length b)) |> Seq.maxBy snd |> fst
+    let nextTarget = matrixOfAngles |> Seq.cast<bool*(double*int)> |> Seq.filter (fun x -> fst x) |> Seq.maxBy snd
+    let mapWithTargetRemoved = matrixOfAngles |> Seq.cast<bool*(double*int)> |> Seq.filter (fun x -> not (x = nextTarget))
     
-    let asteroidsInLineOfFire = matrixOfAngles |> Array2D.mapi (fun y x c -> if fst c && snd c = angleWithMostAsteroids then (true, (y, x)) else (false, (0, 0))) |> Seq.cast<bool*(int*int)> |> Seq.filter (fun x -> fst x) |> Seq.map snd
-    let furthestAsteroid = asteroidsInLineOfFire |> Seq.map (fun (y, x) -> x * 100 + y, abs (x - fst stationLocation) + abs (y - snd stationLocation)) |> Seq.maxBy snd |> fst
+    //let asteroidsInLineOfFire = matrixOfAngles |> Array2D.mapi (fun y x c -> if fst c && snd c = angleWithMostAsteroids then (true, (y, x)) else (false, (0, 0))) |> Seq.cast<bool*(int*int)> |> Seq.filter (fun x -> fst x) |> Seq.map snd
+    //let furthestAsteroid = asteroidsInLineOfFire |> Seq.map (fun (y, x) -> x * 100 + y, abs (x - fst stationLocation) + abs (y - snd stationLocation)) |> Seq.maxBy snd |> fst
 
-    if isDebug then printfn "furthestAsteroid %A angleWithMostAsteroids %A asteroidsInLineOfFire %A matrixOfAngles %A" furthestAsteroid angleWithMostAsteroids asteroidsInLineOfFire matrixOfAngles
-    printfn "furthestAsteroid %A" furthestAsteroid
+    //if isDebug then printfn "furthestAsteroid %A angleWithMostAsteroids %A asteroidsInLineOfFire %A matrixOfAngles %A" furthestAsteroid angleWithMostAsteroids asteroidsInLineOfFire matrixOfAngles
+    printfn "%A %A" nextTarget matrixOfAngles
 
 vaporise(".#....#####...#..
 ##...##.#####..##
