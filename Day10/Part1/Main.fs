16d1aedeefe9d5e6599ddd13d11eb8407f3b7a59
Matthew Latto <matt.salesforce.jl@gmail.com>
2020-11-18 00:01:50 +0000

    Update Main.fs

--- Day10/Part1/Main.fs
+++ Day10/Part1/Main.fs
@@ -1,45 +1,17 @@
 let findBestAsteroid(spaceMap:string) =
-    let isDebug = true
+    let isDebug = false
     let arrays = spaceMap.Split '\010' |> Seq.map (fun a -> Array.ofSeq a) |> Array.ofSeq
     let matrix = Array2D.init arrays.Length arrays.[0].Length (fun i j -> arrays.[i].[j])
 
     if isDebug then printfn "%A" matrix
 
     let countAsteroids(y, x) = 
+        let angles = matrix |> Array2D.mapi (fun v h c -> if c = '#' && not (y = v && x = h) then System.Math.Atan2(float (y - v), float (x - h)) else 0.0)
+        angles |> Seq.cast<double> |> Seq.distinct |> Seq.length
 
-        let up = matrix.[0..(y - 1), x] |> Array.exists ((=) '#') |> (fun b -> if b = true then 1 else 0)
-        let down = matrix.[(y + 1).., x] |> Array.exists ((=) '#') |> (fun b -> if b = true then 1 else 0)
-        let left = matrix.[y, 0..(x - 1)] |> Array.exists ((=) '#') |> (fun b -> if b = true then 1 else 0)
-        let right = matrix.[y, (x + 1)..] |> Array.exists ((=) '#') |> (fun b -> if b = true then 1 else 0)
-    
-        let mutable lowerRight = 0
-        for i in 1 .. min (arrays.Length - y - 1) (arrays.[0].Length - x - 1) do
-            if matrix.[y + i, x + i] = '#' then
-                lowerRight <- 1
-
-        let mutable upperLeft = 0        
-        for i in 1 .. min y x do
-            if matrix.[y - i, x - i] = '#' then
-                upperLeft <- 1
-                   
-        let mutable upperRight = 0
-        for i in 1 .. min y (arrays.[0].Length - x - 1) do
-            if matrix.[y - i, x + i] = '#' then
-                upperRight <- 1
-
-        let mutable lowerLeft = 0
-        for i in 1 .. min (arrays.Length - y - 1) x do
-            if matrix.[y + i, x - i] = '#' then
-                lowerLeft <- 1
-
-        let otherAsteroids = matrix |> Array2D.mapi (fun v h i -> if i = '#' && not (v = y || h = x || (x - h) = (y - v) || (x - h) = -(y - v)) then 1 else 0)  |> Seq.cast<int> |> Seq.sum
-
-        if matrix.[y,x] = '#' then up + down + left + right + upperRight + upperLeft + lowerLeft + lowerRight + otherAsteroids else 0
-
-    let countedAsteroids = matrix |> Array2D.mapi (fun y x a -> countAsteroids(y, x)) |> Seq.cast<int>
-    let asteroidCount = countedAsteroids |> Seq.max
+    let countedAsteroids = matrix |> Array2D.mapi (fun y x c -> if c = '#' then countAsteroids(y, x) else 0) |> Seq.cast<int>
+    let asteroidCount = (countedAsteroids |> Seq.max) - 1
     let location = countedAsteroids |> Seq.findIndex ((=) (countedAsteroids |> Seq.max)) |> fun i -> (i - (i / arrays.[0].Length) * arrays.[0].Length, i / arrays.[0].Length)
-
     printfn "location %O count %i" location asteroidCount
 
 findBestAsteroid(".#..#
@@ -101,3 +73,4 @@ findBestAsteroid(".#..##.###...#######
 .#.#.###########.###
 #.#.#.#####.####.###
 ###.##.####.##.#..##")
+
