bc302ecdf24397ace6fab928479f5ab1182b554b
Matt <matt.salesforce.jl@gmail.com>
2020-12-01 20:42:25 +0000

    Y 2020 D 01 P 1 - minor performance enhancement.

--- 2020/code/Day01Part2.fs
+++ 2020/code/Day01Part2.fs
@@ -9,7 +9,7 @@ module Day01Part2 =
         for position1 in [0..digits.Length - 1] do
             for position2 in [0..digits.Length - 1] do
                 for position3 in [0..digits.Length - 1] do
-                if (digits.[position1] + digits.[position2] + digits.[position3]= 2020) && position1 <> position2 && position2 <> position3 then
+                if answer = 0 && (digits.[position1] + digits.[position2] + digits.[position3]= 2020) && position1 <> position2 && position2 <> position3 then
                     answer <- digits.[position1] * digits.[position2] * digits.[position3] 
 
         answer
