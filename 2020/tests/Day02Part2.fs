5a101fdb78f6d95215bf7903a87419fb3ddb0fbd
Matt <matt.salesforce.jl@gmail.com>
2020-12-02 10:08:12 +0000

    Added unit tests for 2020 Day 2

---
+++ 2020/tests/Day02Part2.fs
@@ -0,0 +1,19 @@
+namespace AoC2020.Tests
+
+open NUnit.Framework
+open AoC2020
+
+[<TestFixture>] 
+type Day02Part2 () =
+
+    [<Test>]
+    member this.Example1() = Assert.AreEqual(1, Day02Part2.checkPasswords ([|"1-3 a: abcde"|]))
+
+    [<Test>]
+    member this.Example2() = Assert.AreEqual(0, Day02Part2.checkPasswords ([|"1-3 b: cdefg"|]))
+
+    [<Test>]
+    member this.Example3() = Assert.AreEqual(0, Day02Part2.checkPasswords ([|"2-9 c: ccccccccc"|]))
+
+    [<Test>]
+    member this.Examples() = Assert.AreEqual(1, Day02Part2.checkPasswords ([|"1-3 a: abcde"; "1-3 b: cdefg"; "2-9 c: ccccccccc"|]))
