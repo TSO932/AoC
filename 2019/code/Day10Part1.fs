6b0a1ccd4dfdf9ab6642ee47475b4c7f4e67e91f
Matt <matt.salesforce.jl@gmail.com>
2020-12-03 17:02:36 +0000

    simplifed syntax for function mapping

--- 2019/code/Day10Part1.fs
+++ 2019/code/Day10Part1.fs
@@ -5,14 +5,14 @@ module Day10Part1 =
 
         let isDebug = false
         
-        let arrays = spaceMap |> Seq.map (fun a -> Array.ofSeq a) |> Array.ofSeq
+        let arrays = spaceMap |> Seq.map (Array.ofSeq) |> Array.ofSeq
         let matrix = Array2D.init arrays.Length arrays.[0].Length (fun i j -> arrays.[i].[j])
 
         if isDebug then printfn "%A" matrix
 
         let countAsteroids(y, x) = 
             let angles = matrix |> Array2D.mapi (fun v h c -> (c = '#' && not (y = v && x = h), System.Math.Atan2(float (y - v), float (x - h))))
-            angles |> Seq.cast<bool*double> |> Seq.filter (fun x -> fst x) |> Seq.distinct |> Seq.length
+            angles |> Seq.cast<bool*double> |> Seq.filter (fst) |> Seq.distinct |> Seq.length
 
         let countedAsteroids = matrix |> Array2D.mapi (fun y x c -> if c = '#' then countAsteroids(y, x) else 0) |> Seq.cast<int>
         let asteroidCount = (countedAsteroids |> Seq.max)
