d13018ccce78b162510537e38dc82430d1a43e8c
Matt <matt.salesforce.jl@gmail.com>
2020-12-07 16:17:29 +0000

    Created templates for tomorrow's challenge.

---
+++ 2020/code/Day08Part1.fs
@@ -0,0 +1,8 @@
+namespace AoC2020
+
+module Day08Part1 =
+    let runProgram (forestMap:seq<string>) =
+
+        let isDebug = true
+        
+        0
---
+++ 2020/tests/Day08Part1.fs
@@ -0,0 +1,11 @@
+namespace AoC2020.Tests
+
+open System.IO
+open NUnit.Framework
+open AoC2020
+
+[<TestFixture>] 
+type Day08Part1 () =
+
+    [<Test>]
+    member this.Example1() = Assert.AreEqual(0, Day08Part1.runProgram (File.ReadAllLines("../../../data/Day08/test1.txt")))
