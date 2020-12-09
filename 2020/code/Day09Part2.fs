7703b790d22da2fe2deac165b1448468d70c8458
Matt <matt.salesforce.jl@gmail.com>
2020-12-09 12:08:11 +0000

    2020 Day 9 Part 2 performance enhancements.

--- 2020/code/Day09Part2.fs
+++ 2020/code/Day09Part2.fs
@@ -7,10 +7,17 @@ module Day09Part2 =
         
         let mutable weakness = 0L
         for start in seq {0..((Array.length intArray) - 1)} do
+            let mutable isBust = false
             for finish in seq {1..(Array.length intArray)} do
-                if weakness = 0L && start <> finish && (intArray.[start..finish] |> Seq.sum) = invalidNumber then
-                    let sortedArray = intArray.[start..finish] |> Array.sort
-                    weakness <- sortedArray.[0] + sortedArray.[sortedArray.Length - 1]
+                if weakness = 0L && not isBust && start <> finish && finish - start < 101 then
+                    match invalidNumber.CompareTo(intArray.[start..finish] |> Seq.sum) with
+                    |  0 -> 
+                        let sortedArray = intArray.[start..finish] |> Array.sort
+                        weakness <- sortedArray.[0] + sortedArray.[sortedArray.Length - 1]
+                    | -1 ->
+                        isBust <- true
+                    |  _ ->
+                        isBust <- false
 
         weakness
 
