d0eed832bf89ed2947561588c94164365fece466
TSO932 <matt.salesforce.jl@gmail.com>
2021-09-26 23:08:37 +0100

    2015 Day 4 Parts 1 & 2 completed.

--- 2015/console/Program.fs
+++ 2015/console/Program.fs
@@ -10,5 +10,7 @@ let main argv =
     printfn "Day  2 Part 1: %i" (Day02Part2.getTotalLength (File.ReadAllLines("../input/Day02/input.txt")))
     printfn "Day  3 Part 1: %i" (Day03Part1.countLuckyHouses (File.ReadAllLines("../input/Day03/input.txt")))
     printfn "Day  3 Part 2: %i" (Day03Part2.countLuckyHouses (File.ReadAllLines("../input/Day03/input.txt")))
+    printfn "Day  4 Part 1: %i" (Day04Part1.getAdventCoin (File.ReadAllLines("../input/Day04/input.txt")))
+    printfn "Day  4 Part 2: %i" (Day04Part2.getAdventCoin (File.ReadAllLines("../input/Day04/input.txt")))
 
     0 // return an integer exit code
