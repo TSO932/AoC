57c95103a1e86ed81941faaf6e93ee42a720616a
TSO932 <matt.salesforce.jl@gmail.com>
2021-10-02 17:04:09 +0100

    Simplify Seq functions.

--- 2015/code/Day05Part2.fs
+++ 2015/code/Day05Part2.fs
@@ -5,10 +5,10 @@ open System.Text.RegularExpressions
 module Day05Part2 =
 
     let isValidRule1 (input:string) =
-         input |> Seq.mapi (fun i _ -> input.[i..]) |> Seq.map (fun s -> s.Length > 3 && s.[2..].Contains(s.[0..1])) |> Seq.reduce max
-         
+         input |> Seq.mapi (fun i _ -> input.[i..]) |> Seq.exists (fun s -> s.Length > 3 && s.[2..].Contains(s.[0..1]))
+
     let isValidRule2 (input:string) =
-         (input |> Seq.windowed 3 |> Seq.filter (fun a -> a.[0] = a.[2]) |> Seq.length ) > 0
+         input |> Seq.windowed 3 |> Seq.exists (fun a -> a.[0] = a.[2])
     
     let isValid (input:string) =
         isValidRule1(input) && isValidRule2(input)
