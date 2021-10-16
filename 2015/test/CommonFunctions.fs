d2dc4e09ea12fa215b43403ced7999092e31fe51
TSO932 <matt.salesforce.jl@gmail.com>
2021-10-16 18:52:03 +0100

    Added a function to get all permutations of a list.

---
+++ 2015/test/CommonFunctions.fs
@@ -0,0 +1,20 @@
+namespace AoC2015.Tests
+
+open NUnit.Framework
+open AoC2015
+
+[<TestFixture>] 
+type CommonFunctions () =
+
+    [<Test>]
+    member _.SingleValue() = CollectionAssert.AreEqual([[1]], CommonFunctions.permute ([1]))
+
+    [<Test>]
+    member _.NoValues() = CollectionAssert.AreEqual([[]], CommonFunctions.permute ([]))
+    
+    [<Test>]
+    member _.TwoIdenticalValues() = CollectionAssert.AreEqual([[[2; 1]]], CommonFunctions.permute ([[2; 1]]))
+
+    [<Test>]
+    member _.ThreeValues() = 
+        CollectionAssert.AreEqual([[1; 2; 3]; [2; 1; 3]; [2; 3; 1]; [1; 3; 2]; [3; 1; 2]; [3; 2; 1]], CommonFunctions.permute ([1; 2; 3]))
