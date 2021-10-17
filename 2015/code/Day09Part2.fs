9f0fdb5c9b2fd012512780811aa3c858f377e345
TSO932 <matt.salesforce.jl@gmail.com>
2021-10-17 12:48:04 +0100

    2015 Day 9 - Tidy up code.

--- 2015/code/Day09Part2.fs
+++ 2015/code/Day09Part2.fs
@@ -1,24 +1,14 @@
 namespace AoC2015
 
-open System.Collections.Generic
-
 module Day09Part2 =
 
     let findLongestDistance(distances:seq<string>) =
         let dists = distances |> Seq.map Day09Part1.parseDistance
 
-        let distDic = new Day09Part1.DistanceDictionary()
+        let distDic = Day09Part1.DistanceDictionary()
         distDic.Populate(dists)
 
-        let rec distribute e = function
-        | [] -> [[e]]
-        | x::xs' as xs -> (e::xs)::[for xs in distribute e xs' -> x::xs]
-
-        let rec permute = function
-        | [] -> [[]]
-        | e::xs -> List.collect (distribute e) (permute xs)
-
-        permute(Day09Part1.getListOfCities(dists)) |> Seq.map ((fun route -> Seq.pairwise route ) >> (fun seqOfPairs -> seqOfPairs |> Seq.sumBy (fun ab -> distDic.Get(ab)))) |> Seq.max
+        CommonFunctions.permute(Day09Part1.getListOfCities(dists)) |> Seq.map (Seq.pairwise >> (fun seqOfPairs -> seqOfPairs |> Seq.sumBy (fun ab -> distDic.Get(ab)))) |> Seq.max
 
         
                 
