63a762c04a28c9b99c4c52bc9277ea7e2a50a484
TSO932 <matt.salesforce.jl@gmail.com>
2021-11-08 17:22:21 +0000

    2015 Day 11 Parts 1 & 2 Completed.

--- 2015/console/Program.fs
+++ 2015/console/Program.fs
@@ -24,7 +24,9 @@ let main argv =
     printfn "Day  9 Part 1: %i" (Day09Part1.findShortestDistance (File.ReadAllLines("../input/Day09/input.txt")))
     printfn "Day  9 Part 2: %i" (Day09Part2.findLongestDistance (File.ReadAllLines("../input/Day09/input.txt")))
     printfn "Day 10 Part 1: %i" (Day10Part2.lookAndSayRepeat (40, File.ReadAllLines("../input/Day10/input.txt").[0]))
-    printfn "Day 10 Part 2: %i" (Day10Part2.lookAndSayRepeat (50, File.ReadAllLines("../input/Day10/input.txt").[0]))
     //printfn "Day 10 Part 1 Elapsed Milliseconds: %f " (PerformanceMeasure.measurePerformance(Day10Part1.lookAndSayRepeat, (25, File.ReadAllLines("../input/Day10/input.txt").[0])))
+    printfn "Day 10 Part 2: %i" (Day10Part2.lookAndSayRepeat (50, File.ReadAllLines("../input/Day10/input.txt").[0]))
+    printfn "Day 11 Part 1: %s" (Day11Part1.getNextPassword (File.ReadAllLines("../input/Day11/input.txt").[0]))
+    printfn "Day 11 Part 2: %s" (Day11Part2.getNextPassword (File.ReadAllLines("../input/Day11/input.txt").[0]))
 
     0 // return an integer exit code
---
+++ 2015/test/Day11Part1.fs
@@ -0,0 +1,124 @@
+namespace AoC2015.Tests
+
+open System
+open NUnit.Framework
+open AoC2015
+
+[<TestFixture>] 
+type Day11Part1 () =
+
+    [<Test>]
+    member _.IncrementPasswordSimple() = Assert.AreEqual("aab", Day11Part1.incrementPassword ("aaa"))
+
+    [<Test>]
+    member _.IncrementPasswordEmpty() = Assert.AreEqual("", Day11Part1.incrementPassword (""))
+
+    [<Test>]
+    member _.IncrementPasswordFlips() = Assert.AreEqual("aabaaa", Day11Part1.incrementPassword ("aaazzz"))
+
+    [<Test>]
+    member _.IncrementPasswordMax() = Assert.AreEqual("aa", Day11Part1.incrementPassword ("zz"))
+
+    [<Test>]
+    member _.generatePassword() = 
+        let dummyValidationRule(password:string) =
+            not (password = "ab")
+
+        Assert.AreEqual("ac", Day11Part1.generatePassword ("aa", dummyValidationRule))
+
+    [<Test>]
+    member _.RuleTwoValid() = Assert.AreEqual(true, Day11Part1.ruleTwo ("aaaaa"))
+
+    [<Test>]
+    member _.RuleTwoOneI() = Assert.AreEqual(false, Day11Part1.ruleTwo ("aaiaa"))
+
+    [<Test>]
+    member _.RuleTwoTwoOs() = Assert.AreEqual(false, Day11Part1.ruleTwo ("oaaao"))
+    
+    [<Test>]
+    member _.RuleTwoSingleL() = Assert.AreEqual(false, Day11Part1.ruleTwo ("l"))
+
+    [<Test>]
+    member _.RuleTwoEmptyIsValid() = Assert.AreEqual(true, Day11Part1.ruleTwo (""))
+
+    [<Test>]
+    member _.RuleThreeNoRepeatsInvalid() = Assert.AreEqual(false, Day11Part1.ruleThree ("abcd"))
+    
+    [<Test>]
+    member _.RuleThreeEmptyInvalid() = Assert.AreEqual(false, Day11Part1.ruleThree (""))
+
+    [<Test>]
+    member _.RuleThreeFourQsIsValid() = Assert.AreEqual(true, Day11Part1.ruleThree ("qqqq"))
+
+    [<Test>]
+    member _.RuleThreeThreeQsIsInvalid() = Assert.AreEqual(false, Day11Part1.ruleThree ("qqq"))
+
+    [<Test>]
+    member _.RuleThreeTwoQsIsInvalid() = Assert.AreEqual(false, Day11Part1.ruleThree ("qq"))
+
+    [<Test>]
+    member _.RuleThreeTwoQsAndTwoRsIsValid() = Assert.AreEqual(true, Day11Part1.ruleThree ("qqrr"))
+
+    [<Test>]
+    member _.RuleThreeTwoQsPaddingAtTheFrontIsValid() = Assert.AreEqual(true, Day11Part1.ruleThree ("aqqqq"))
+
+    [<Test>]
+    member _.RuleThreeTwoQsPaddingAtInTheMiddleIsValid() = Assert.AreEqual(true, Day11Part1.ruleThree ("qqaqq"))
+
+    [<Test>]
+    member _.RuleThreeTwoQsPaddingAtAtTheEndIsValid() = Assert.AreEqual(true, Day11Part1.ruleThree ("qqqqa"))
+
+    [<Test>]
+    member _.RuleThreeTwoQsPaddingIsValid() = Assert.AreEqual(true, Day11Part1.ruleThree ("abcqqdeqqfg"))
+
+    [<Test>]
+    member _.isStraightInvalidFalling() = Assert.AreEqual(false, Day11Part1.isStraight ([|'c'; 'b'; 'a'|]))
+
+    [<Test>]
+    member _.isStraightInvalidLast() = Assert.AreEqual(false, Day11Part1.isStraight ([|'a'; 'b'; 'd'|]))
+
+    [<Test>]
+    member _.isStraightInvalidFirst() = Assert.AreEqual(false, Day11Part1.isStraight ([|'a'; 'c'; 'd'|]))
+
+    [<Test>]
+    member _.isStraightValidExample1() = Assert.AreEqual(true, Day11Part1.isStraight ([|'a'; 'b'; 'c'|]))
+
+    [<Test>]
+    member _.isStraightValidExample2() = Assert.AreEqual(true, Day11Part1.isStraight ([|'b'; 'c'; 'd'|]))
+
+    [<Test>]
+    member _.isStraightValidExample3() = Assert.AreEqual(true, Day11Part1.isStraight ([|'c'; 'd'; 'e'|]))
+
+    [<Test>]
+    member _.isStraightValidExample4() = Assert.AreEqual(true, Day11Part1.isStraight ([|'x'; 'y'; 'z'|]))
+
+    [<Test>]
+    member _.isStraightTooShort() = Assert.Throws<IndexOutOfRangeException> (fun() -> Assert.AreEqual(false, Day11Part1.isStraight ([||])) |> ignore) |> ignore
+
+    [<Test>]
+    member _.RuleOneEmptyIsInvalid() = Assert.AreEqual(false, Day11Part1.ruleOne (""))
+
+    [<Test>]
+    member _.RuleOneInvalid() = Assert.AreEqual(false, Day11Part1.ruleOne ("the quick brown fox jumped over the lazy dog. aabbccddeeffgg!"))
+    
+    [<Test>]
+    member _.RuleOneValid() = Assert.AreEqual(true, Day11Part1.ruleOne ("qwertyjkluiop"))
+
+    [<Test>]
+    member _.RulesExample1() = Assert.AreEqual(false, Day11Part1.rules ("hijklmmn"))
+
+    [<Test>]
+    member _.RulesExample2() = Assert.AreEqual(false, Day11Part1.rules ("abbceffg"))
+
+    [<Test>]
+    member _.RulesExample3() = Assert.AreEqual(false, Day11Part1.rules ("abbcegjk"))
+
+    [<Test>]
+    member _.RulesValid() = Assert.AreEqual(true, Day11Part1.ruleOne ("aabcdeff"))
+
+    [<Test>]
+    member _.GetNextPasswordExample1() = Assert.AreEqual("abcdffaa", Day11Part1.getNextPassword ("abcdefgh"))
+
+    [<Test>]
+    member _.GetNextPasswordExample2() = Assert.AreEqual("ghjaabcc", Day11Part1.getNextPassword ("ghijklmn"))
+
