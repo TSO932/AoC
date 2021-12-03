1c8bb076abfcb5a1e8aaad95250da5d3a9a4bb52
TSO932 <matt.salesforce.jl@gmail.com>
2021-12-03 11:52:20 +0000

    2021 Day 3 Part 1 complete.

--- 2021/code/Day03Part1.fs
+++ 2021/code/Day03Part1.fs
@@ -5,24 +5,26 @@ module Day03Part1 =
 
         let diagnostic = input |> Seq.map (fun d -> Seq.toList d) 
 
-        let mostCommonFirstDigit(input:seq<list<char>>, tiebreak:int) =
+        let getMostCommonAndLeastCommonFirstDigit(input:seq<list<char>>) =
             let groupByStartingDigit = input |> Seq.groupBy Seq.head
 
             let getCountOfNs(n:char) = groupByStartingDigit |> Seq.filter (fun (c, _) -> c = n) |> Seq.exactlyOne |> snd |> Seq.length
 
             match getCountOfNs('1').CompareTo(getCountOfNs('0')) with
-            |  1 -> 1
-            | -1 -> 0
-            |  _ -> tiebreak      
+            | -1 -> (0, 1)
+            | _  -> (1, 0)
 
-        let rec getmostCommonDigits(input:seq<list<char>>, tiebreak:int, result:seq<int>) =
+        let rec getMostCommonAndLeastCommonDigits(input:seq<list<char>>, result:seq<int*int>) =
             if input |> Seq.head |> Seq.length = 0 then
                 result
             else
-                getmostCommonDigits(Seq.map List.tail input, tiebreak, Seq.append result [mostCommonFirstDigit(input, tiebreak)])
+                getMostCommonAndLeastCommonDigits(Seq.map List.tail input, Seq.append result [getMostCommonAndLeastCommonFirstDigit(input)])
         
-        let getDecimal(bits:seq<int>) = bits |> Seq.fold (fun a b -> a * 2 + b) 0 
-        getDecimal( getmostCommonDigits(diagnostic, 1, []) )
+        let getDecimal(bits:seq<int>) = bits |> Seq.fold (fun a b -> a * 2 + b) 0
+
+        let mostCommonAndLeastCommonDigits = getMostCommonAndLeastCommonDigits(diagnostic, []) 
+        
+        getDecimal(Seq.map fst mostCommonAndLeastCommonDigits) * getDecimal(Seq.map snd mostCommonAndLeastCommonDigits)
 
 
 
--- 2021/console/Program.fs
+++ 2021/console/Program.fs
@@ -7,4 +7,5 @@ let main argv =
     printfn "Day  1 Part 1: %i" (Day01Part1.countDepthIncreases (File.ReadAllLines("../input/Day01/input.txt")))
     printfn "Day  1 Part 2: %i" (Day01Part2.countDepthIncreases (File.ReadAllLines("../input/Day01/input.txt")))
 
+    printfn "Day  3 Part 1: %A" (Day03Part1.run (File.ReadAllLines("../input/Day03/input.txt")))
     0 // return an integer exit code 
