e7205e988e1b22bd8bf211cb60656603a50bcd98
TSO932 <matt.salesforce.jl@gmail.com>
2021-12-03 11:40:59 +0000

    2021 Day 1 Part 1 work in progress.

---
+++ 2021/code/Day03Part1.fs
@@ -0,0 +1,29 @@
+﻿namespace AoC2021
+
+module Day03Part1 =
+    let run (input:seq<string>) =
+
+        let diagnostic = input |> Seq.map (fun d -> Seq.toList d) 
+
+        let mostCommonFirstDigit(input:seq<list<char>>, tiebreak:int) =
+            let groupByStartingDigit = input |> Seq.groupBy Seq.head
+
+            let getCountOfNs(n:char) = groupByStartingDigit |> Seq.filter (fun (c, _) -> c = n) |> Seq.exactlyOne |> snd |> Seq.length
+
+            match getCountOfNs('1').CompareTo(getCountOfNs('0')) with
+            |  1 -> 1
+            | -1 -> 0
+            |  _ -> tiebreak      
+
+        let rec getmostCommonDigits(input:seq<list<char>>, tiebreak:int, result:seq<int>) =
+            if input |> Seq.head |> Seq.length = 0 then
+                result
+            else
+                getmostCommonDigits(Seq.map List.tail input, tiebreak, Seq.append result [mostCommonFirstDigit(input, tiebreak)])
+        
+        let getDecimal(bits:seq<int>) = bits |> Seq.fold (fun a b -> a * 2 + b) 0 
+        getDecimal( getmostCommonDigits(diagnostic, 1, []) )
+
+
+
+
