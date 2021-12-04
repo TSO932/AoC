3bd0a121facb6f1a060403318f78b0727c95a2e2
TSO932 <matt.salesforce.jl@gmail.com>
2021-12-04 14:38:09 +0000

    2021 Day 4 completed.

--- 2021/test/Day04Part1.fs
+++ 2021/test/Day04Part1.fs
@@ -7,22 +7,22 @@ open AoC2021
 type Day04Part1 () =
 
     [<Test>]
-    member _.CheckForBingoNo() = Assert.AreEqual(-1, Day04Part1.checkForBingo(array2D [["1"; "2"; "3"; "4"; "5"];
-                                                                                          ["6"; "7"; "8"; "9"; "10"];
-                                                                                          ["1"; "2"; "*"; "4"; "5"];
-                                                                                          ["6"; "*"; "8"; "9"; "10"];
-                                                                                          ["6"; "7"; "8"; "9"; "10"]]))
+    member _.CheckForBingoNo() = Assert.AreEqual(false, fst (Day04Part1.checkForBingo(array2D [["1"; "2"; "3"; "4"; "5"];
+                                                                                               ["6"; "7"; "8"; "9"; "10"];
+                                                                                               ["1"; "2"; "*"; "4"; "5"];
+                                                                                               ["6"; "*"; "8"; "9"; "10"];
+                                                                                               ["6"; "7"; "8"; "9"; "10"]])))
 
                                                                                      
     [<Test>]
-    member _.CheckForBingoYesVertical() = Assert.AreEqual(120, Day04Part1.checkForBingo(array2D [["1"; "2"; "*"; "4"; "5"];
-                                                                                                  ["6"; "7"; "*"; "9"; "10"];
-                                                                                                  ["1"; "2"; "*"; "4"; "5"];
-                                                                                                  ["6"; "7"; "*"; "9"; "10"];
-                                                                                                  ["6"; "7"; "*"; "9"; "10"]]))
+    member _.CheckForBingoYesVertical() = Assert.AreEqual((true, 120), Day04Part1.checkForBingo(array2D [["1"; "2"; "*"; "4"; "5"];
+                                                                                                         ["6"; "7"; "*"; "9"; "10"];
+                                                                                                         ["1"; "2"; "*"; "4"; "5"];
+                                                                                                         ["6"; "7"; "*"; "9"; "10"];
+                                                                                                         ["6"; "7"; "*"; "9"; "10"]]))
     [<Test>]
-    member _.CheckForBingoYesHorizontal() = Assert.AreEqual(135, Day04Part1.checkForBingo(array2D [["1"; "2"; "3"; "4"; "5"];
-                                                                                                   ["6"; "7"; "8"; "9"; "10"];
-                                                                                                   ["*"; "*"; "*"; "*"; "*"];
-                                                                                                   ["6"; "7"; "8"; "9"; "10"];
-                                                                                                   ["6"; "7"; "8"; "9"; "10"]]))
+    member _.CheckForBingoYesHorizontal() = Assert.AreEqual((true, 135), Day04Part1.checkForBingo(array2D [["1"; "2"; "3"; "4"; "5"];
+                                                                                                           ["6"; "7"; "8"; "9"; "10"];
+                                                                                                           ["*"; "*"; "*"; "*"; "*"];
+                                                                                                           ["6"; "7"; "8"; "9"; "10"];
+                                                                                                           ["6"; "7"; "8"; "9"; "10"]]))
