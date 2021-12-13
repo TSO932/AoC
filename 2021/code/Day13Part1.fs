6948a8788b773fdda33dd59ce88822cb46c9c1a6
TSO932 <matt.salesforce.jl@gmail.com>
2021-12-13 23:33:28 +0000

    2021 Day 13 Part 1 completed.

--- 2021/code/Day13Part1.fs
+++ 2021/code/Day13Part1.fs
@@ -15,18 +15,14 @@ module Day13Part1 =
         let paper = Array2D.create height width false
 
         coords |> Seq.iter (fun (x, y) -> Array2D.set paper y x true)
-
-        printfn "%A" paper
-        printfn "%i %i" width height
-
         paper
     
     let xFold(x:int, paper:bool[,]) =
         let left  = paper[*, .. x - 1]
         let right = paper[*, x + 1 ..]
-        let width  = (Array2D.length2 right) - 1
+        let width  = (Array2D.length2 left) - 1
 
-        let folded = Array2D.copy right
+        let folded = Array2D.copy left
         right |> Array2D.mapi (fun y x b -> Array2D.set folded y (width - x) (b || Array2D.get left y (width - x))) |> ignore
         folded
     
@@ -35,7 +31,7 @@ module Day13Part1 =
         let bottom = paper[y + 1 .., *]
         let height  = (Array2D.length1 top) - 1
 
-        let folded = Array2D.copy bottom
+        let folded = Array2D.copy top
         bottom |> Array2D.mapi (fun y x b -> Array2D.set folded (height - y) x (b || Array2D.get top (height - y) x)) |> ignore
         folded
 
