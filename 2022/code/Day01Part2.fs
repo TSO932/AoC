b40ed8ef60c8893ac1118bc1f55a11f124a3cf27
TSO932 <matt.salesforce.jl@gmail.com>
2022-12-01 16:00:18 +0000

    2022 Day 1

---
+++ 2022/code/Day01Part2.fs
@@ -0,0 +1,13 @@
+﻿namespace AoC2022
+
+module Day01Part2 =
+    let FindElvesCarryingMostCalories (input:seq<string>) =
+        input
+            |> Seq.scan (fun accum entry -> if entry = "" then (fst accum + 1, 0) else (fst accum, int entry)) (0, 0)
+            |> Seq.groupBy fst
+            |> Seq.map snd
+            |> Seq.map (fun s -> s |> Seq.sumBy snd ) 
+            |> Seq.sort
+            |> Seq.rev
+            |> Seq.take 3
+            |> Seq.sum
---
+++ 2022/console/Program.fs
@@ -0,0 +1,10 @@
+﻿open System.IO
+open AoC2022
+
+[<EntryPoint>]
+let main argv =
+
+    printfn "Day  1 Part 1: %A" (Day01Part1.FindElfCarryingMostCalories (File.ReadAllLines("../input/Day01/input.txt")))
+    printfn "Day  1 Part 2: %A" (Day01Part2.FindElvesCarryingMostCalories (File.ReadAllLines("../input/Day01/input.txt")))
+
+    0 // return an integer exit code 
