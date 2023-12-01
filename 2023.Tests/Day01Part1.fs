942f3dd5b635374bc8c46d80b40698612ee5a136
TSO932 <matt.salesforce.jl@gmail.com>
2023-12-01 06:23:56 +0000

    2023 Day 1 Part 2 work in progress

--- 2023.Tests/Day01Part1.fs
+++ 2023.Tests/Day01Part1.fs
@@ -8,3 +8,6 @@ type Day01Part1 () =
 
     [<Test>]
     member _.Example() = Assert.AreEqual(142, Day01Part1.findElfCarryingMostCalories [|"1abc2"; "pqr3stu8vwx"; "a1b2c3d4e5f"; "treb7uchet"|])
+
+    [<Test>]
+    member _.Example2() = Assert.AreEqual(281, Day01Part1.findElfCarryingMostCalories2 [|"two1nine"; "eightwothree"; "abcone2threexyz"; "xtwone3four"; "4nineeightseven2"; "zoneight234"; "7pqrstsixteen"|])
--- 2023/Day01Part1.fs
+++ 2023/Day01Part1.fs
@@ -7,8 +7,32 @@ module Day01Part1 =
         |> Seq.map (Seq.filter (fun x -> x >= '0' && x <= '9'))
         |> Seq.map (fun q -> (q |> Seq.head |> System.Char.ToString ) + (q |> Seq.rev |> Seq.head |> System.Char.ToString))
         |> Seq.sumBy System.Int32.Parse
-        
-        // + (Seq.rev >> Seq.head))
 
+    let findElfCarryingMostCalories2 (input: seq<string>) =
+        input
+        |> Seq.map (fun x -> x
+                                .Replace("oneight", "1ight")
+                                .Replace("threeight", "3ight")
+                                .Replace("fiveight", "5ight")
+                                .Replace("nineight", "9ight")
+                                .Replace("eightwo", "8wo")
+                                .Replace("eighthree","8hree")
+                                .Replace("twone", "2ne")
+                                .Replace("sevenine","7ine")
+                                .Replace("one", "1")
+                                .Replace("two", "2")
+                                .Replace("three", "3")
+                                .Replace("four", "4")
+                                .Replace("five", "5")
+                                .Replace("six", "6")
+                                .Replace("seven", "7")
+                                .Replace("eight", "8")
+                                .Replace("nine", "9"))
+                                // .Replace("zero", "0"))
+
+        |> Seq.map Seq.toList
+        |> Seq.map (Seq.filter (fun x -> x >= '0' && x <= '9'))
+        |> Seq.map (fun q -> (q |> Seq.head |> System.Char.ToString ) + (q |> Seq.rev |> Seq.head |> System.Char.ToString))
+        |> Seq.sumBy System.Int32.Parse
 
 
