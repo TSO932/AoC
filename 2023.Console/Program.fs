6f8e5db05d6390a1489871688f01e6fe5e17be0a
TSO932 <matt.salesforce.jl@gmail.com>
2023-12-02 17:52:11 +0000

    2023 Day 2 Part 2

--- 2023.Console/Program.fs
+++ 2023.Console/Program.fs
@@ -4,3 +4,4 @@ open _2023
 printfn "Day  1 Part 1: %A" (Day01Part1.calibrate (File.ReadAllLines("../2023.Input/Day01/input.txt")))
 printfn "Day  1 Part 2: %A" (Day01Part2.calibrate (File.ReadAllLines("../2023.Input/Day01/input.txt")))
 printfn "Day  2 Part 1: %A" (Day02Part1.gameSum (File.ReadAllLines("../2023.Input/Day02/input.txt")))
+printfn "Day  2 Part 2: %A" (Day02Part2.cubePowers (File.ReadAllLines("../2023.Input/Day02/input.txt")))
---
+++ 2023/Day02Part2.fs
@@ -0,0 +1,17 @@
+﻿namespace _2023
+
+module Day02Part2 =
+
+    let getCubeCount = Day02Part1.getCubeCount
+
+    let getMaxByColour (colour:string, input:seq<string*int>) =
+        input
+        |> Seq.filter (fun x ->  fst x = colour)
+        |> Seq.maxBy snd
+        |> snd
+
+    let cubePowers (input: seq<string>) =
+            input
+            |> Seq.map (fun x -> x.Split [|':'; ','; ';'|])
+            |> Seq.map (fun x -> Seq.map getCubeCount (Seq.tail x))
+            |> Seq.sumBy (fun x -> getMaxByColour ("blue", x) * getMaxByColour ("red", x) * getMaxByColour ("green", x))
