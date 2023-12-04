15736d0f71300744b823f9e8a7b1f66129b2573e
TSO932 <matt.salesforce.jl@gmail.com>
2023-12-04 18:25:17 +0000

    2023 Day 4

--- 2023.Console/Program.fs
+++ 2023.Console/Program.fs
@@ -5,3 +5,5 @@ printfn "Day  1 Part 1: %A" (Day01Part1.calibrate (File.ReadAllLines("../2023.In
 printfn "Day  1 Part 2: %A" (Day01Part2.calibrate (File.ReadAllLines("../2023.Input/Day01/input.txt")))
 printfn "Day  2 Part 1: %A" (Day02Part1.gameSum (File.ReadAllLines("../2023.Input/Day02/input.txt")))
 printfn "Day  2 Part 2: %A" (Day02Part2.cubePowers (File.ReadAllLines("../2023.Input/Day02/input.txt")))
+printfn "Day  4 Part 1: %A" (Day04Part1.getSum (File.ReadAllLines("../2023.Input/Day04/input.txt")))
+printfn "Day  4 Part 2: %A" (Day04Part2.getSum (File.ReadAllLines("../2023.Input/Day04/input.txt")))
---
+++ 2023/Day04Part2.fs
@@ -0,0 +1,51 @@
+﻿namespace _2023
+
+open System.Collections.Generic
+
+module Day04Part2 =
+
+    let getNewCards (input:string) =
+ 
+        let numbers =
+            input[5..].Split [|':'; '|'|]
+            |> Seq.map (fun s -> s.Split ' ')
+            |> Seq.map (fun q -> Seq.filter ((<>) "") q)
+            |> Seq.toArray
+
+        let cardNumber =
+            numbers[0]
+            |> Seq.head
+            |> int
+
+        numbers[1]
+        |> Seq.filter (fun n1 -> numbers[2] |> Seq.exists (fun n2 -> n1 = n2))
+        |> Seq.length
+        |> (fun n -> (cardNumber, seq { cardNumber + 1 .. cardNumber + n}))
+
+    let getSum (input:seq<string>) =
+        
+        let cardMap =
+            input
+            |> Seq.map getNewCards
+            |> Map
+
+        let startingCards = Seq.length input
+
+        let cardCounts = new Dictionary<int, int>()
+        seq {1 .. Seq.length input}
+        |> Seq.iter (fun c -> cardCounts.Add (c, 1))
+
+        let play (cardNumber) =
+            let updateCount (playedCard, wonCard) =
+                let count = cardCounts[wonCard] + cardCounts[playedCard]
+                cardCounts.Remove (wonCard) |> ignore
+                cardCounts.Add (wonCard, count)
+
+            cardMap[cardNumber] |> Seq.iter (fun c -> updateCount (cardNumber, c))
+                
+        seq {1 .. startingCards}
+        |> Seq.iter play
+
+        seq {1 .. startingCards}
+        |> Seq.sumBy (fun c -> cardCounts[c])
+        
