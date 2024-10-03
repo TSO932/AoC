ba4e11c767161d4d7a908421c69751dba3b6326b
TSO932 <matt.salesforce.jl@gmail.com>
2024-10-03 17:15:03 +0100

    2017 Day 3 Part 2

--- 2017/code/Day03Part2.fs
+++ 2017/code/Day03Part2.fs
@@ -1,29 +1,39 @@
 namespace AoC2017
 
 module Day03Part2 =
-    let getVal(targetValue) =
+    let getVal (targetValue) =
 
+        // Create a large grid of 0s and set the middle value to 1
         let grid = Array2D.create 101 101 0
         Array2D.set grid 50 50 1
 
-        let sumSurroundingValues (x, y) =
-            List.sum [ grid[x - 1, y - 1]; grid[x, y - 1]; grid[x + 1, y - 1]; grid[x - 1, y]; grid[x + 1, y]; grid[x - 1, y + 1]; grid[x, y + 1]; grid[x + 1, y + 1] ]
+        let rec walk (n, x, y, dir) =
 
-        let rec walk(n, x, y, dir) =
+            let sumSurroundingValues (x, y) =
+                List.sum
+                    [ grid[x - 1, y - 1]
+                      grid[x, y - 1]
+                      grid[x + 1, y - 1]
+                      grid[x - 1, y]
+                      grid[x + 1, y]
+                      grid[x - 1, y + 1]
+                      grid[x, y + 1]
+                      grid[x + 1, y + 1] ]
 
-            let nextValue = sumSurroundingValues(x, y)
+            let nextValue = sumSurroundingValues (x, y)
 
             if nextValue > targetValue then
                 nextValue
             else
                 Array2D.set grid x y nextValue
 
-                let nextDir = 
+                let nextDir =
                     match dir with
-                        | (1, 0) -> (0, 1)
-                        | (0, 1) -> (-1, 0)
-                        | (-1, 0) -> (0, -1)
-                        | _ -> (1, 0)
+                    | (1, 0) -> (0, 1)
+                    | (0, 1) -> (-1, 0)
+                    | (-1, 0) -> (0, -1)
+                    | _ -> (1, 0)
+
                 let nextX = x + fst nextDir
                 let nextY = y + snd nextDir
 
@@ -32,9 +42,9 @@ module Day03Part2 =
                         (n + 1, nextX, nextY, nextDir)
                     else
                         (n + 1, x + fst dir, y + snd dir, dir)
-            
+
                 walk nextInstruction
 
-        walk (2, 51, 50, (1,0))
+        walk (2, 51, 50, (1, 0))
 
     let getValue (input: seq<string>) = input |> Seq.head |> int |> getVal
