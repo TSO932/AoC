442c7c4602b60df258f6954324815d6725f9dc1c
TSO932 <matt.salesforce.jl@gmail.com>
2024-12-03 00:30:08 +0000

    2024 Day 2 Part 1

--- 2024.Console/Program.fs
+++ 2024.Console/Program.fs
@@ -3,3 +3,4 @@ open _2024
 
 printfn "Day  1 Part 1: %i" (Day01Part1.run (File.ReadAllLines("../2024.Input/Day01/input.txt")))
 printfn "Day  1 Part 2: %i" (Day01Part2.run (File.ReadAllLines("../2024.Input/Day01/input.txt")))
+printfn "Day  2 Part 1: %i" (Day02Part1.run (File.ReadAllLines("../2024.Input/Day02/input.txt")))
---
+++ 2024/Day02Part1.fs
@@ -0,0 +1,35 @@
+﻿namespace _2024
+
+open System
+
+module Day02Part1 =
+
+    let isValid(input:string) =
+        let seq1 =
+            input.Split ' '
+            |> Seq.map Int32.Parse
+
+        let seqA =
+            seq1
+            |> Seq.rev
+            |> Seq.tail
+            |> Seq.rev
+
+        let seqB = Seq.tail seq1
+
+        let diffs =
+            (seqA, seqB)
+            ||> Seq.zip
+            |> Seq.map (fun (a, b) -> b - a)
+        
+        let maxDiff = Seq.max diffs
+        let minDiff = Seq.min diffs
+
+        ( (maxDiff > 0 && minDiff > 0)  || (maxDiff < 0 && minDiff < 0) ) && maxDiff <= 3 && minDiff >= -3
+
+
+    let run (input: seq<string>) =
+        
+        input
+        |> Seq.filter isValid
+        |> Seq.length
