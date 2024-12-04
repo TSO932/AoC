deff963dcc51325f1cbe32ca768086366f40576b
TSO932 <matt.salesforce.jl@gmail.com>
2024-12-04 23:04:54 +0000

    2024 Day 3 Part 2 - Simpler Method

--- 2024.Tests/Day03Part2.fs
+++ 2024.Tests/Day03Part2.fs
@@ -5,42 +5,22 @@ open _2024
 
 [<TestFixture>]
 type Day03Part2 () =
+ 
+    [<Test>]
+    member _.getEnabledInstructionFalse() =
+        Assert.That(Day03Part2.getEnabledInstruction(("hat", false),"ham"), Is.EqualTo(("mul(0,0)", false)))
 
     [<Test>]
-    member _.getDos() =
-        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-        Assert.That(Day03Part2.getDos input, Is.EqualTo([(59, true)]))
-    
-    [<Test>]
-    member _.getDonts() =
-        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-        Assert.That(Day03Part2.getDonts input, Is.EqualTo([(20, false)]))
+    member _.getEnabledInstructionTrue() =
+        Assert.That(Day03Part2.getEnabledInstruction(("hat", true),"ham"), Is.EqualTo(("ham", true)))
 
     [<Test>]
-    member _.getDosAndDonts() =
-        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-        Assert.That(Day03Part2.getDosAndDonts input, Is.EqualTo([(20, false); (59, true)]))
+    member _.getEnabledInstructionDo() =
+        Assert.That(Day03Part2.getEnabledInstruction(("hat", true),"do()"), Is.EqualTo(("mul(0,0)", true)))
 
     [<Test>]
-    member _.isIndexPositionEnabled00() =
-        let input = [(20, false); (59, true)]
-        Assert.That(Day03Part2.isIndexPositionEnabled (input, 0), Is.EqualTo(true))
-
-    [<Test>]
-    member _.isIndexPositionEnabled30() =
-        let input = [(20, false); (59, true)]
-        Assert.That(Day03Part2.isIndexPositionEnabled (input, 30), Is.EqualTo(false))
-
-    [<Test>]
-    member _.isIndexPositionEnabled90() =
-        let input = [(20, false); (59, true)]
-        Assert.That(Day03Part2.isIndexPositionEnabled (input, 90), Is.EqualTo(true))
-
-    [<Test>]
-    member _.getEnabledInstructions() =
-        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
-        let expected = "xmul(2,4)&mul[3,7]!^do()?mul(8,5))"
-        Assert.That(Day03Part2.getEnabledInstructions input, Is.EqualTo(expected))      
+    member _.getEnabledInstructionDont() =
+        Assert.That(Day03Part2.getEnabledInstruction(("hat", true),"don't()"), Is.EqualTo(("mul(0,0)", false)))
 
     [<Test>]
     member _.getSum() =
--- 2024/Day03Part2.fs
+++ 2024/Day03Part2.fs
@@ -5,43 +5,23 @@ open System.Text.RegularExpressions
 
 module Day03Part2 =
 
-    let getDos(input:string) =
-        Regex.Matches(input, "do\(\)")
-        |> Seq.map (fun a -> a.Index)
-        |> Seq.map (fun i -> (i, true))
+    let skipInst = "mul(0,0)"
 
-    let getDonts(input:string) =
-        Regex.Matches(input, "don't\(\)")
-        |> Seq.map (fun a -> a.Index)
-        |> Seq.map (fun i -> (i, false))
+    let getEnabledInstruction ((_, isEnabled:bool), input:string) =
+        match input with
+        | "don't()" -> (skipInst, false)
+        | "do()" -> (skipInst, true)
+        | _ -> if isEnabled then (input, true) else (skipInst, false)
 
-    let getDosAndDonts(input:string) =
-        Seq.append (getDos(input)) (getDonts(input))
-        |> Seq.sortBy snd
-
-    let isIndexPositionEnabled(rules, i) =
-        rules
-        |> Seq.filter (fun (j, _) -> j < i)
-        |> Seq.append [(-1, true)]
-        |> Seq.sortBy fst
-        |> Seq.last
-        |> snd
-
-    let getEnabledInstructions(input:string) =
-
-        let rules = getDosAndDonts(input)
-
-        input
-        |> Seq.toArray
-        |> Array.mapi (fun i c -> (c, isIndexPositionEnabled (rules, i)))
-        |> Array.filter (fun (_, b) -> b)
-        |> Array.map fst
-        |> System.String
+    let getInstructions(input:string) =
+        Regex.Matches(input, "mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)")
+        |> Seq.scan (fun acc a -> getEnabledInstruction (acc, a.Value)) (skipInst, true)
+        |> Seq.filter snd
+        |> Seq.map fst
 
     let getSum(input:string) =
         input
-        |> getEnabledInstructions
-        |> Day03Part1.getInstructions
+        |> getInstructions
         |> Seq.sumBy Day03Part1.getMultiple
 
     let run(input:seq<string>) =
